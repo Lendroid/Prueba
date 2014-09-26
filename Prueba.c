f591c14c23da32830a788ca75dcebef58f48ec37
lendroid <kamikaze.1951@hotmail.com>
2014-09-26 01:37:30 -0500

    Se agrega suma

--- Prueba.c
+++ Prueba.c
@@ -4,6 +4,34 @@
 int main()
 {
 printf("Hola mundo\n");
+double a,b,r;
+//Incluimos Calculadora Basica
+
+printf("Ingrese un numero:\n");
+scanf("%d",&a);
+
+printf("Ingrese otro numero:\n");
+scanf("%d",&a);
+
+printf("\nLos resultados son lo siguientes\n");
+printf("\nSuma:\n");
+r=a+b;
+printf("%d",r);
+
+printf("\nResta:\n");
+r=a-b;
+printf("%d",r);
+
+printf("\nMultiplicacion:\n");
+r=a*b;
+printf("%d",r);
+
+printf("\nDivicion:\n");
+r=a/b;
+printf("%i",r);
+
+printf("\nGracias,buen dia\n");
+
 system ("pause");
 return 0;
 
